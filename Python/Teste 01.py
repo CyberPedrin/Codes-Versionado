a6cf5c279a930bfc05a3164e06c960b33bc163fc
Pedro Henrique <phmendes211@gmail.com>
2026-06-16 18:12:28 -0300

    Adicionei outras pastas

---
+++ Python/Teste 01.py
@@ -0,0 +1,14 @@
+from rich import print
+from rich.panel import Panel
+from rich.traceback import install
+install()
+from rich import inspect    
+from rich.console import Console
+from rich.table import Table
+import os
+os.system("cls")
+from time import sleep
+from abc import ABC, abstractmethod
+
+#Comece daqui!
+
